f252439e7adc6aee3494eb74cae8302ca0379597
Artem Yur <artemonodon@gmail.com>
2026-06-29 13:20:08 +0200

    'Solution'

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -113,6 +113,11 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+}
+
 AUTH_USER_MODEL = "user.User"
 
 # Internationalization
@@ -124,7 +129,7 @@ TIME_ZONE = "UTC"
 
 USE_I18N = True
 
-USE_TZ = False
+USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
